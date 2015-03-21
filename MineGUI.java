2daed9a6b284d4eec395bc61079a179e388e4549
papasy228 <papasy228@gmail.com>
2015-03-20 20:02:27 -0400

    added files
    Added  minesweeper model and gui

---
+++ MineGUI.java
@@ -0,0 +1,42 @@
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JFrame;
+
+/*
+ * MineGUI.java 
+ *
+ * Version:
+ *     $Id:$
+ * 
+ * Revisions: 
+ *     $Log:$ 
+ *
+ *
+ *
+ *@author  Papa Yaw Ntorinkansah 
+ */
+
+
+public class MineGUI extends JFrame implements Observer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3251734441780419997L;
+
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 System.out.println("g is null");
+
+	}
+
+}
